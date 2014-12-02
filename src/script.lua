b588175a955b63d9f0ba26e92359d8aa21d5def5
Yonaba <roland.yonaba@gmail.com>
2014-12-02 11:30:17 +0000

    Added entry point

---
+++ src/script.lua
@@ -0,0 +1,48 @@
+--[[/*
+  =============================================================================
+  =																		                                        =
+  =					             UPLINK For PSP					                              =
+  =																			                                      =
+  =							 Started August '10							                              =
+  =																			                                      =
+  =					  Source Code: Roland Y.                                          =
+  =                                                                           =
+  =					Uses 'Uplink Hacker Elite' Original Graphics                      =
+  =																			                                      =
+  =																			                                      =
+  =============================================================================
+  */
+--]]
+
+--Pilot File : Inits all modules
+
+--lphmEmu is not executed on PSP, just on PC for debug
+if os.getenv("os")=='Windows_NT' then
+	require './Contents/Scripts/lphmEmu'
+end
+
+
+--Disables ME to use old Soud and Music Funcs	
+System.oaenable()
+	
+--Loads Libraries
+require './Contents/Scripts/Constants'	
+require './Contents/Scripts/Sounds'	
+require './Contents/Scripts/Audio'	
+require './Contents/Data/Identities/ids'	
+require './Contents/Scripts/Core'
+require './Contents/Scripts/Cursor'
+require './Contents/scripts/GUI'
+require './Contents/scripts/news'
+require './Contents/scripts/resource'
+require './Contents/scripts/uplink'
+require './Contents/Data/Files/files'
+require './Contents/scripts/server'
+require './Contents/scripts/bank'
+require './Contents/scripts/federal'
+require './Contents/scripts/Time'
+require './Contents/Scripts/HUD'
+require './Contents/scripts/missions'
+require './Contents/GameCore/GameCore'
+
+System.oadisable()
