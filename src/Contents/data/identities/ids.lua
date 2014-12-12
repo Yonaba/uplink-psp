1ef6401a7de7c7c3b366d7a1212f1571bd82f8de
Yonaba <roland.yonaba@gmail.com>
2014-12-12 11:35:56 +0000

    Added data

---
+++ src/Contents/data/identities/ids.lua
@@ -0,0 +1,32 @@
+--[[ Uplink For PSP
+	Identities definition File
+	 - Creates all characters id's
+	]]
+	
+People={
+{name="Allan",name2="Wilson",pass="burdenage",acc="1229",img=Image.load("./Contents/graphics/photos/Image1.jpg")},
+{name="John",name2="Baxter",pass="rosebud",acc="2891",img=Image.load("./Contents/graphics/photos/Image2.jpg")},
+{name="Vance",name2="Courtney",pass="hotroad",acc="1203",img=Image.load("./Contents/graphics/photos/Image4.jpg")},
+{name="Vick",name2="King",pass="systemfuck",acc="3467",img=Image.load("./Contents/graphics/photos/Image5.jpg")},
+{name="Merick",name2="Chapman",pass="iamtheking",acc="1093",img=Image.load("./Contents/graphics/photos/Image6.jpg")},
+{name="Jonathan",name2="Mackenzie",pass="ilovemywife",acc="2083",img=Image.load("./Contents/graphics/photos/Image10.jpg")},
+{name="Timothy",name2="Boss",pass="inthehood",acc="4562",img=Image.load("./Contents/graphics/photos/Image11.jpg")},
+{name="Nguyen",name2="Xuan",pass="holybible",acc="6352",img=Image.load("./Contents/graphics/photos/Image12.jpg")},
+{name="Rashi",name2="Al KAshi",pass="spiritson",acc="9365",img=Image.load("./Contents/graphics/photos/Image13.jpg")},
+{name="Xao",name2="Minh",pass="inheritance",acc="5262",img=Image.load("./Contents/graphics/photos/Image3.jpg")},
+{name="Philip",name2="Redmond",pass="program",acc="6252",img=Image.load("./Contents/graphics/photos/Image18.jpg")},
+{name="Mackenzie",name2="Cristopher",pass="device",acc="9827",img=Image.load("./Contents/graphics/photos/Image17.jpg")},
+{name="Paul",name2="Leroy",pass="universal",acc="0928",img=Image.load("./Contents/graphics/photos/Image20.jpg")},
+{name="Tobey",name2="Ming Li Fo",pass="keycontrol",acc="8965",img=Image.load("./Contents/graphics/photos/Image15.jpg")},
+{name="Lakshmi",name2="Mitaal",pass="holdmenow",acc="4456",img=Image.load("./Contents/graphics/photos/Image13.jpg")},
+{name="Harold",name2="Caine",pass="shawnofdead",acc="1111",img=Image.load("./Contents/graphics/photos/Image8.jpg")},
+{name="Thomas",name2="Smith",pass="triadice",acc="5262",img=Image.load("./Contents/graphics/photos/Image3.jpg")},
+{name="Aloysus",name2="Bourne",pass="aloysusbourne",acc="8257",img=Image.load("./Contents/graphics/photos/Image9.jpg")},
+{name="Arthur",name2="Belley",pass="operationdelta",acc="7625",img=Image.load("./Contents/graphics/photos/Image10.jpg")},
+{name="Ben",name2="Freeman",pass="dumbdumb",acc="9272",img=Image.load("./Contents/graphics/photos/Image20.jpg")},
+{name="Alexander",name2="Mitnick",pass="heavily",acc="6278",img=Image.load("./Contents/graphics/photos/Image19.jpg")},
+{name="Marvin",name2="Randall",pass="bulletproof",acc="0092",img=Image.load("./Contents/graphics/photos/Image18.jpg")},
+{name="Aaron",name2="Vidic",pass="mycaddie",acc="9289",img=Image.load("./Contents/graphics/photos/Image4.jpg")},
+{name="Bob",name2="Benton",pass="foxtrot",acc="2567",img=Image.load("./Contents/graphics/photos/Image0.jpg")},
+{name="Roger",name2="Davies",pass="federal",acc="2823",img=Image.load("./Contents/graphics/photos/Image9.jpg")},
+}
