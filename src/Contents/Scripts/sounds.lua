fd0713451cf1c5917288c490e2b04fd86c513aa1
Yonaba <roland.yonaba@gmail.com>
2014-12-15 11:37:40 +0000

    Added sounds

---
+++ src/Contents/Scripts/sounds.lua
@@ -0,0 +1,12 @@
+--[[ Uplink For PSP
+	Sounds Loader 
+	]]
+	
+--Sounds loader
+
+AddLink = Sound.load('./Contents/audio/sounds/addLinkBounce.wav',false)
+Close = Sound.load('./Contents/audio/sounds/Close.wav',false)
+Done = Sound.load('./Contents/audio/sounds/done.wav',false)
+Login = Sound.load('./Contents/audio/sounds/login.wav',false)
+ExecuteSoft = Sound.load('./Contents/audio/sounds/runsoftware.wav',false)
+--TraceBeep = Sound.load('./Contents/audio/sounds/tracebleep.wav',true)
